386e13ae1cfa27d7e3046f35f9b76459a2695876
Simon Shields <simon@lineageos.org>
2017-09-04 21:09:44 +1000

    soong: explicitly specify name of custom variables struct
    Change-Id: Ic754b8a13658ddc40620f985581bd1754b780b15

--- android/variable.go
+++ android/variable.go
@@ -98,7 +98,7 @@ type variableProperties struct {
 		}
 
         // include custom variables
-        *android.Product_variables
+        custom android.Product_variables
 	} `android:"arch_variant"`
 }
 
@@ -176,7 +176,7 @@ type productVariables struct {
 	BoardUsesQTIHardware *bool `json:",omitempty"`
 
     // include custom variables
-    *android.ProductVariables
+    custom android.ProductVariables
 }
 
 func boolPtr(v bool) *bool {
